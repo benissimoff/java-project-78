479d75eda7307d1dcc6514b39debf82076d32df6
ben <186181126+benissimoff@users.noreply.github.com>
2025-04-24 00:45:08 +0300

    fix: DesignForExtension
    Class 'BaseSchema' looks like designed for extension (can be subclassed), but the method 'isValid' does not have javadoc that explains how to do that safely. If class is not designed for extension consider making the class 'BaseSchema' final or making the method 'isValid' static/final/abstract/empty, or adding allowed annotation for the method. [DesignForExtension]
    Error: eckstyle] [ERROR] /home/runner/work/java-project-78/java-project-78/app/src/main/java/hexlet/code/schemas/BaseSchema.java:20:5: Class 'BaseSchema' looks like designed for extension (can be subclassed), but the method 'addCheck' does not have javadoc that explains how to do that safely. If class is not designed for extension consider making the class 'BaseSchema' final or making the method 'addCheck' static/final/abstract/empty, or adding allowed annotation for the method. [DesignForExtension]

--- app/src/main/java/hexlet/code/schemas/BaseSchema.java
+++ app/src/main/java/hexlet/code/schemas/BaseSchema.java
@@ -6,7 +6,7 @@ import java.util.function.Function;
 
 public abstract class BaseSchema<T> {
     private final List<Function<T, Boolean>> checks = new ArrayList<>();
-    public boolean isValid(T param) {
+    public final boolean isValid(T param) {
         for (var check : checks) {
             var result = !check.apply(param);
             if (result) {
@@ -17,7 +17,7 @@ public abstract class BaseSchema<T> {
         return true;
     }
 
-    protected void addCheck(Function<T, Boolean> checkFunction) {
+    protected final void addCheck(Function<T, Boolean> checkFunction) {
         checks.add(checkFunction);
     }
 }
